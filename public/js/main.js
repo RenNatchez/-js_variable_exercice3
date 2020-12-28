b243f563e0a1e3f7254ee839952812f755e03b95
natchez <natchezzz@hotmail.com>
2020-12-28 15:49:07 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let signe = prompt("a ou b ou c?")
+
+if (signe.toLowerCase() == "b") {
+    console.log("mauvaise réponse")
+    }
+
+if (signe.toLowerCase() == "a") {
+    console.log("bonne réponse")
+    }
